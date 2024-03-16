d3a6936057695df7ab037f269aea8a7102264548
abhishekch33 <abhishek.2101134ec@iiitbh.ac.in>
2024-03-17 00:23:17 +0530

    modi

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -45,7 +45,7 @@ const projectsData = [
     name: "E-Commerce Website : Abhishek's Amazon",
     tags: ["web-page"],
     media: {
-      thumbnail: require("../../images/portfolio/Ecommerce.png"),
+      thumbnail: require("../../images/portfolio/ecommerce.png"),
     },
   },
   {
--- src/components/blogs/index.jsx
+++ src/components/blogs/index.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import Section from "../shared/section/Section";
 import leetcode from "../../images/blogs/leetcode.png";
 import codeforces from "../../images/blogs/codeforces.jpg";
-import codingNinja from "../../images/blogs/CodingNinja.png";
+import codingNinja from "../../images/blogs/codingninja.png";
 import codeChef from "../../images/blogs/codechef.jpeg";
 import BlogCard from "./blog-card";
 
